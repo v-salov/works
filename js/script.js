647f5dd42eec7bf793dd104f7413803601638f93
Вадим <vadim@MBP-Vadim.kit2005.com.ua>
2019-08-05 18:01:03 +0300

    Script

--- js/script.js
+++ js/script.js
@@ -13,4 +13,21 @@ $(window).load(function () {
   $('.menu-button').on('click', function() {
     $('.menu').toggleClass('menu_active')
   })
+
+  //Настройка select
+  $('.select__checked').on('click', function() {
+    $('.select__dropdown').toggleClass('select__dropdown_open')
+  })
+  $('.select__option').on('click', function() {
+    let value = $(this).attr('data-value');
+    $('#select-type').val(value);
+    $('.select__checked').text(value);
+    $('.select__dropdown').toggleClass('select__dropdown_open');
+  })
+
+  $("a[href^='#']").on('click', function() {
+    let _href = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(_href).offset().top-130 + 'px'});
+    return false;
+  })
 });
